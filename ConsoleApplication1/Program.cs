2af8f615dd26e230b0703dfe10244620a3330262
MadlenStoicheva <infinite.love@abv.bg>
2016-11-29 16:09:11 +0200

    new

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime thisDate = new DateTime(2000, 6, 2);
+            Console.WriteLine("Today is"+ thisDate.ToString("MM dd, yyyy") + ".");
+
+            string currentMonth = string.Format("");
+        }
+    }
+}
--- egn-366zad/Program.cs
+++ egn-366zad/Program.cs
@@ -10,11 +10,13 @@ namespace egn_366zad
     {
         static void Main(string[] args)
         {
+            // wsqka cifra   0*2,1*4,2*8,3*5,4*10,5*9,6*7,7*3,8*6 /11 = 2
+            //    2485109736.
             //EGN 8406194582 - 1984
             //EGN 7552011038 - 2075
             //EGN 7524169268 - 1875
 
-            string myEgn = "7552011038";
+            string myEgn = "2405189736";
 
             int years = int.Parse(myEgn.Substring(0, 2));
             int months = int.Parse(myEgn.Substring(2, 2));
@@ -32,16 +34,50 @@ namespace egn_366zad
             }
             else if (months > 19 && months < 33)
             {
-                Console.WriteLine("18" + years);
+                months -= 20;
+                Console.Write("18" + years);
+                Console.WriteLine(".{0}" , months);
+                Console.WriteLine(".{0}.{1}", months, days);
+                
             }
             else if (months > 40 && months < 53)
             {
+                months -= 40;
                 Console.WriteLine("20" + years);
+                Console.WriteLine(".{0}", months);
+                Console.WriteLine(".{0}.{1}", months, days);
             }
             else
             {
                 Console.WriteLine("Wrong Data!!! ");
+
             }
+
+            checkDigitalWeight(myEgn);
+        }
+
+        private static void checkDigitalWeight(string egn)
+        {
+            
+            int[] digit = new int[egn.Length];
+
+            for (int i = 0; i < digit.Length; i++)
+            {
+                digit[i] = egn[i];
+                
+            }
+
+            int currentSum = (digit[0] *= 2) + (digit[1] *= 4) + (digit[2] *= 8) + (digit[3] *= 5) + (digit[4] *= 10) + (digit[5] *= 9) + (digit[6] *= 7) + (digit[7] *= 3) + (digit[8] *= 6);
+            Console.WriteLine(currentSum);
+            currentSum = currentSum % 11;
+            
+          
+            if (currentSum == 10)
+            {
+                currentSum = 0;
+            }
+            Console.WriteLine(11 - currentSum);
+            
         }
 
         public static bool IsFemale(string genderDigit)
